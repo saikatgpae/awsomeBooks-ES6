0a95000d940dc6860998032b9e65d9f6369a947a
Saikat Chakraborty <saikatgpae@gmail.com>
2022-08-16 14:25:31 +0530

    Change the normal function to arrow function.

--- module/deleteRow.js
+++ module/deleteRow.js
@@ -1,15 +1,15 @@
 import { Books } from './book.js';
 
-function deleteRow() {
-  const index = this.previousElementSibling.innerHTML;
+const deleteRow = (e) => {
+  const index = e.currentTarget.previousElementSibling.innerHTML;
   const books = new Books();
   const booksStored = JSON.parse(localStorage.getItem('books'));
   books.list = booksStored;
   books.removeItem(index);
   localStorage.setItem('books', JSON.stringify(books.list));
   /* eslint-disable */
-    location.reload();
-    /* eslint-enable */
+  location.reload();
+  /* eslint-enable */
 }
 /* eslint-disable */
 export { deleteRow };
